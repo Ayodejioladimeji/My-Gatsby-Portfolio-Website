6c0251c95c587848040e4635566da39c14ba8f92
Ayodejioladimeji <layobright11@gmail.com>
2020-12-28 00:10:41 -0800

    favicon

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
-import Favicon from '../../public/layomi.png'
+import Favicon from '../layomi.png'
 
 const query = graphql`
   {
